9504322afcab54453a51e41edcb5e2c528c9e9f8
phil <phil@secdev.org>
2020-11-05 19:52:30 +0100

    Release 0.2

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 #! /usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
+
 
 setup(
     name = 'cakeutils',
-    version = '0.1',
+    version = '0.2',
     packages=['cakeutils'],
     # Metadata
     author = 'Philippe BIONDI',
-    author_email = 'phil(at)secdev.org',
-    description = 'Cake utils: many utilitary functions to make the cake',
+    author_email = 'phil@secdev.org',
+    description = 'Cake utils: tools to make the cake',
     license = 'GPLv2',
     # keywords = '',
     url = 'http://www.secdev.org/projects/cakeutils/',
